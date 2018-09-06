f663fa3b4e69e390defad73c6440b841986925f5
Joanna Gilberti <jgilberti@archemy.com>
2018-09-06 14:28:36 -0400

    updates

--- src/components/GlobalNav.jsx
+++ src/components/GlobalNav.jsx
@@ -6,16 +6,7 @@ export default () => (
   <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
     <div className="navbar-brand">
       <Link className="navbar-item" to="/">
-		render() {
-			return (
-				<div className="row">
-					<div className="logo">
-						<img src={logo} width="100" height="50" />
-					</div>
-				</div>
-			);
-		}
-        <h2 className="title has-text-white">Active Business Solutions Network Workbench Client</h2>
+		<img src={require('../img/Archemy_TM.png')}/><h2 className="title has-text-white">Active Business Solutions Network Workbench Client</h2>
       </Link>
     </div>
   </nav>
