3e80364af266ad52bdf706c26c5a84424b2227f8
Joanna Gilberti <jgilberti@archemy.com>
2018-09-06 16:40:36 -0400

    updates

--- src/components/Shortcuts.jsx
+++ src/components/Shortcuts.jsx
@@ -4,14 +4,14 @@ export default () => (
   <aside className="section">
     <h4 className="is-size-4">Shortcuts</h4>
     <div className="buttons">
-      <button className="button" type="button">EA Mgmt. Toolkit </button>
-      <button className="button" type="button">Knowledge & Search Tool </button>
-      <button className="button" type="button">Steps Documentation Tool </button>
-      <button className="button" type="button">Visualization Tool</button>
-      <button className="button" type="button">Utilities</button>
-      <button className="button" type="button">Workbench Blog</button>
-      <button className="button" type="button">Planet EPA</button>
-      <button className="button" type="button">Workbench Forum</button>
+      <button className="button is-light" type="button">EA Mgmt. Toolkit </button>
+      <button className="button is-light" type="button">Knowledge & Search Tool </button>
+      <button className="button is-light" type="button">Steps Documentation Tool </button>
+      <button className="button is-light" type="button">Visualization Tool</button>
+      <button className="button is-light" type="button">Utilities</button>
+      <button className="button is-light" type="button">Workbench Blog</button>
+      <button className="button is-light" type="button">Planet EPA</button>
+      <button className="button is-light" type="button">Workbench Forum</button>
     </div>
   </aside>
 );
