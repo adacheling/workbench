0e562478ee000ed87f64bbd746ecf6b2ee7de36d
Joanna Gilberti <jgilberti@archemy.com>
2018-09-06 16:35:38 -0400

    updates

--- src/components/GlobalNav.jsx
+++ src/components/GlobalNav.jsx
@@ -6,9 +6,11 @@ import logo from '../img/Archemy_TM.png';
 export default () => (
   <nav className="navbar" role="navigation" aria-label="main navigation">
     <div className="navbar-brand">
-      <Link className="navbar-item" to="/">
-		<img src={require('../img/Archemy_TM.png')}/><h4 className="title">Active Business Solutions Network Workbench Client</h4>
-      </Link>
+      <!--<Link className="navbar-item" to="/">-->
+	  <div className="navbar-item">
+		<img src={require('../img/Archemy_TM.png')}/><h3 className="title">Active Business Solutions Network Workbench Client</h3>
+	  </div>
+      <!--</Link>-->
     </div>
   </nav>
 );
