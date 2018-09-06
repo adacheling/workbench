3f470a6a085b159b48992232747881ff3000c7b4
Joanna Gilberti <jgilberti@archemy.com>
2018-09-06 14:54:13 -0400

    updates

--- src/components/GlobalNav.jsx
+++ src/components/GlobalNav.jsx
@@ -5,7 +5,7 @@ import logo from '../img/Archemy_TM.png';
 import './App.css';
 
 export default () => (
-  <nav className="navbar is-dark" style="background-color: #1F5F71;" role="navigation" aria-label="main navigation">
+  <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
     <div className="navbar-brand">
       <Link className="navbar-item" to="/">
 		<img src={require('../img/Archemy_TM.png')}/><h3 className="title has-text-white">Active Business Solutions Network Workbench Client</h3>
