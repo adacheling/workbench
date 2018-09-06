2ad3f039a8abf2261d8625783dca8daf12a2dafa
Joanna Gilberti <jgilberti@archemy.com>
2018-09-06 14:25:29 -0400

    updates

--- src/components/GlobalNav.jsx
+++ src/components/GlobalNav.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../img/Archemy_TM.png';
 
 export default () => (
   <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
     <div className="navbar-brand">
       <Link className="navbar-item" to="/">
-        <h1 className="title has-text-white">Active Business Solutions Network Workbench Client</h1>
+		render() {
+			return (
+				<div className="row">
+					<div className="logo">
+						<img src={logo} width="100" height="50" />
+					</div>
+				</div>
+			);
+		}
+        <h2 className="title has-text-white">Active Business Solutions Network Workbench Client</h2>
       </Link>
     </div>
   </nav>
