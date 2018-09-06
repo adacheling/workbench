025495dcb7e3474521c9b17d298b499ee473739d
Joanna Gilberti <jgilberti@archemy.com>
2018-09-06 17:35:16 -0400

    updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,11 +6,13 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 export default () => (
   <footer className="footer">
     <div className="container">
-      <div className="content has-text-left">
-        <p>
-          <copyright>&copy; 2018 Archemy, Inc. or its affiliates</copyright>
-        </p>
-      </div>
+       <div class="small-print">
+            <div class="container">
+                <span class="pull-left">
+       	            <copyright>&copy; 2018 Archemy, Inc. or its affiliates</copyright>
+                </span>
+            </div>
+        </div>
     </div>
   </footer>
 );
