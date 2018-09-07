e1162bf9581c6e6cf6322ac4d43c19bab9624a0b
Joanna Gilberti <jgilberti@archemy.com>
2018-09-06 20:31:03 -0400

    updates

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,6 @@
 import React               from 'react';
 import PropTypes           from 'prop-types';
+import './App.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome }          from '@fortawesome/free-solid-svg-icons';
