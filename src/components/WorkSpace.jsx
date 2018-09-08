7b2a2710f1bdae73dabf3a23b2136b596211bdf2
Joanna Gilberti <jgilberti@archemy.com>
2018-09-08 17:52:21 -0400

    updates

--- src/components/WorkSpace.jsx
+++ src/components/WorkSpace.jsx
@@ -2,28 +2,27 @@ import React  from 'react';
 import { Link } from 'react-router-dom';
 
 export default () => (
-  <div className="container is-fluid">
-
-    <div className="columns">
-      <div className="column is-two-thirds">
-        <Search />
-      </div>
-      <div className="column">
-        <section className="section">
-          <h2 className="title">Business Solution Connections</h2>
-          <button type="button" className="button is-large">LymphMeter</button>
-          <button type="button" className="button is-large">beta</button>
-          <button type="button" className="button is-large">gamma</button>
-
-        </section>
-        <section className="section">
-          <h3 className="is-size-3">Business Solution Architectures </h3>
-          <button type="button" className="button is-large">alpha</button>
-          <button type="button" className="button is-large">beta</button>
-          <button type="button" className="button is-large">gamma</button>
-
-        </section>
-      </div>
-    </div>
-  </div>
+  <form className="box">
+	<div className="container is-fluid">
+		<div className="columns">
+			<div className="column is-two-thirds">
+				<Search />
+			</div>
+			<div className="column">
+				<section className="section">
+					<h2 className="title">Business Solution Connections</h2>
+					<button type="button" className="button is-large">LymphMeter</button>
+					<button type="button" className="button is-large">beta</button>
+					<button type="button" className="button is-large">gamma</button>
+				</section>
+				<section className="section">
+					<h3 className="is-size-3">Business Solution Architectures </h3>
+					<button type="button" className="button is-large">alpha</button>
+					<button type="button" className="button is-large">beta</button>
+					<button type="button" className="button is-large">gamma</button>
+				</section>
+			</div>
+		</div>
+	</div>
+  </form>
 );
