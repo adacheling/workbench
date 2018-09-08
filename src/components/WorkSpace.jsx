a7cd5b6fb8dd74f5ea6f029bc43eddc57b0ed812
Joanna Gilberti <jgilberti@archemy.com>
2018-09-08 18:17:55 -0400

    updates

--- src/components/WorkSpace.jsx
+++ src/components/WorkSpace.jsx
@@ -8,15 +8,27 @@ export default () => (
 			<div className="column">
 				<section className="section">
 					<h2 className="title">Business Solution Connections</h2>
-					<button type="button" className="button is-large">LymphMeter</button>
-					<button type="button" className="button is-large">beta</button>
-					<button type="button" className="button is-large">gamma</button>
+					<table class="table table-borderless">
+						<tbody>
+							<tr>
+								<td><button type="button" className="button">LymphMeter™ v1.1</button></td>
+								<td><button type="button" className="button">LymphMeter™ v1.2</button></td>
+								<td><button type="button" className="button">LymphMeter™ v1.3</button></td>
+							</tr>
+							<tr>
+								<td><font color="#c60b21">Ready</font></td>
+								<td><font color="#c60b21">Ready</font></td>
+								<td><font color="#c60b21">Ready</font></td>
+							</tr>
+						</tbody>
+					</table>
 				</section>
 				<section className="section">
-					<h3 className="is-size-3">Business Solution Architectures </h3>
-					<button type="button" className="button is-large">alpha</button>
-					<button type="button" className="button is-large">beta</button>
-					<button type="button" className="button is-large">gamma</button>
+					<h2 className="title">Business Solution Architectures</h2>
+					<button type="button" className="button">LymphMeter™ v1.0</button>
+					<button type="button" className="button">LymphMeter™ v1.1</button>
+					<button type="button" className="button">LymphMeter™ v1.2</button>
+					<button type="button" className="button">LymphMeter™ v1.3</button>
 				</section>
 			</div>
 		</div>
