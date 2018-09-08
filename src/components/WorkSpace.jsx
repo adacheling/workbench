b5cbac5245399f88eeba168c6a21b8448c8bedf5
Joanna Gilberti <jgilberti@archemy.com>
2018-09-08 18:59:10 -0400

    updates

--- src/components/WorkSpace.jsx
+++ src/components/WorkSpace.jsx
@@ -1,5 +1,6 @@
 import React  from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 export default () => (
