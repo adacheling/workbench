c7b6fcf259c56e6b72757d531b613d44ece39b58
Joanna Gilberti <jgilberti@archemy.com>
2018-09-10 19:49:25 -0400

    updates

--- src/components/Elicit.jsx
+++ src/components/Elicit.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+function handleChange(e) {
+  console.log(this.state)
+  this.setState({selectedCompany: e.target.value})
+},
+
 export default () => (
+    companies:[
+      { name: 'company1',  jobs: ['job1-1', 'job1-2', 'job1-3']},
+      { name: 'company2', jobs: ['job2-1', 'job2-2', 'job2-3']},
+      { name: 'company3', jobs: ['job3-1', 'job3-2', 'job3-3']}
+    ],
+    selectedCompany: 'company1'
+	
+	let company = this.state.companies.filter(company => {
+              return company.name === this.state.selectedCompany
+            })
+
   <form className="box">
     <p className="subtitle is-4">Select strategic roadmap:</p>
     <div className="select">
+	
+	jobs
+        <select>
+          {
+            
+            company[0].jobs.map((job, i) => {
+              return <option>{job}</option>
+            })
+          }
+        </select>
+        
+        companies
+        <select value={this.state.selectedCompany} onChange={this.handleChange.bind(this)}>
+          {
+            this.state.companies.map((company, i) => {
+              return <option>{company.name}</option>
+            })
+          }
+        </select>
+	
+	
       <select>
         <option>stg-rmp – 1/10/17</option>
         <option>stg-rmp – 1/10/18</option>
