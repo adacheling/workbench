03bac02ecb2c334f8981bbf29758b447958b4ea4
Joanna Gilberti <jgilberti@archemy.com>
2018-09-10 21:01:07 -0400

    updates

--- src/components/Elicit.jsx
+++ src/components/Elicit.jsx
@@ -8,11 +8,11 @@ export default () => (
      name: 'React',
      selectedOption: {},
      selectedOption2: {},
-   };
+   }
 
   handleChange1 = (selectedOption) => {
     this.setState({selectedOption});
-  };
+  }
 
   handleChange2 = (selectedOption) => {
     this.setState({selectedOption2: selectedOption})
