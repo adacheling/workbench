8a5dbe018689db3639defb5f1696afdaea35402a
Joanna Gilberti <jgilberti@archemy.com>
2018-09-27 12:30:54 -0400

    heroku updates

--- src/components/TabsOperate.jsx
+++ src/components/TabsOperate.jsx
@@ -19,12 +19,12 @@ function buildNav(nav, path) {
 }
 
 export default ({ nav, path }) => {
-  const { Operate } = nav.find(obj => ('Operate' in obj));
+  const { Build } = nav.find(obj => ('Operate' in obj));
 
   return (
     <div className="tabs">
       <ul>
-        {buildNav(Operate, path)}
+        {buildNav(Build, path)}
       </ul>
     </div>
   );
