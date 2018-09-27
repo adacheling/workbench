39fc55070b76b9699a574d4d425fb0f86a7cb024
Joanna Gilberti <jgilberti@archemy.com>
2018-09-26 22:38:46 -0400

    heroku updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 /* eslint-env browser */
-import 'bootstrap/dist/css/bootstrap.min.css';
+/*import 'bootstrap/dist/css/bootstrap.min.css';*/
 import React    from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
