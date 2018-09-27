277578162f0ada722b06bf9e278b93fc6239024e
Joanna Gilberti <jgilberti@archemy.com>
2018-09-27 11:31:46 -0400

    heroku updates

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -72,10 +72,16 @@ class App extends React.Component {
                     </span>
                   )}
                 />
-				<Route exact path="/solution/operate" component={Operate} />
-				<Route exact path="/solution/operaterun" component={OperateRun} />
-				<Route exact path="/solution/visualize" component={Visualize} />
-				<Route exact path="/solution/visreview" component={VisReview} />
+				  <span>
+					<TabsOperate
+						nav={navSwitch(props.location.pathname)}
+						path={props.location.pathname}
+                    />
+					<Route exact path="/solution/operate" component={Operate} />
+					<Route exact path="/solution/operaterun" component={OperateRun} />
+					<Route exact path="/solution/visualize" component={Visualize} />
+					<Route exact path="/solution/visreview" component={VisReview} />
+				  </span>
               </Switch>
             </main>
             <aside className="column is-one-quarter">
