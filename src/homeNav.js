d5690737560815daa0467bd1404eac8b7b5679c0
Joanna Gilberti <jgilberti@archemy.com>
2018-09-27 11:54:35 -0400

    heroku updates

--- src/homeNav.js
+++ src/homeNav.js
@@ -73,6 +73,15 @@ module.exports = (key) => {
 	case 'solution/visreview':
       return build;
 
+module.exports = (key) => {
+  switch (key) {
+	case 'solution':
+	case '/solution/operate':
+	case '/solution/operaterun':
+	case 'solution/visualize':
+	case 'solution/visreview':
+      return operate;
+
     default:
       return home;
   }
